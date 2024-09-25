5c38473f765a8efbd6369f0dd5dbcb4d52af28b3
Suno <leski@naver.com>
2024-09-25 23:43:49 +0900

    first commit

---
+++ src/main/java/org/skeleton2024/Skeleton2024Application.java
@@ -0,0 +1,13 @@
+package org.skeleton2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skeleton2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skeleton2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/skeleton2024/Skeleton2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skeleton2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skeleton2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
